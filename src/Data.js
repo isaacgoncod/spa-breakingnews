81344195fb545fc41a4673c229018677c7dc84f9
isaacgoncod <isaacgoncod@gmail.com>
2023-04-10 20:24:00 -0300

    create component navbar

---
+++ src/Data.js
@@ -0,0 +1,26 @@
+export const news = [
+  {
+    title: "Noticia 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    image:
+      "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+  },
+];
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import logo from "../../images/LogoBN.png";
+import "./Navbar.css";
+
+export function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="input-search-space">
+          <i className="bi bi-search"></i>
+          <input type="text" placeholder="Pesquise por um título" />
+        </div>
+        <img src={logo} alt="Logo Breaking News" />
+        <button>Entrar</button>
+      </nav>
+    </>
+  );
+}
